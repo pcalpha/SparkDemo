ca354b510dc43b9cf422f5744ea4bdb20ceaee86
caiyida <caiyida@hikvision.com>
2018-12-11 10:39:07 +0800

    [MOD]新增两个简单的demo

---
+++ src/main/scala/cn/com/pcalpha/spark_demo/example/WordCount.scala
@@ -0,0 +1,41 @@
+package cn.com.pcalpha.spark_demo.example
+
+import org.apache.spark.{SparkConf, SparkContext}
+import scala.util.matching.Regex
+
+object WordCount {
+  val PARTTERN: Regex ="^\\w+$".r;
+
+  def main(args: Array[String]): Unit = {
+    val conf  = new SparkConf().setAppName("WordCount").setMaster("local[3]")
+    val context = new SparkContext(conf)
+    val textFile = context.textFile("testFile\\Apache Kafka.html");
+    textFile.flatMap(line=>line.split(" "))
+      .filter(word=>isValidateWord(word))
+      .map(word=>(word,1))
+      .reduceByKey((x,y)=>x+y)
+      .map{case(x,y)=>(y,x)}
+      .sortByKey(false)
+      .top(10)
+      .foreach(e=>println(e._1+" "+e._2))
+
+    print("==============")
+    var max = textFile.flatMap(line=>line.split(" "))
+      .filter(word=>isValidateWord(word))
+      .map(word=>(word,1))
+      .reduceByKey((x,y)=>x+y)
+      .map{case(x,y)=>(y,x)}
+      .sortByKey(false).first()
+    print(max)
+  }
+
+  def isValidateWord(line: String): Boolean = {
+    val options = PARTTERN.findFirstMatchIn(line)
+
+    if (options.isEmpty) {
+      false
+    } else {
+      true
+    }
+  }
+}
