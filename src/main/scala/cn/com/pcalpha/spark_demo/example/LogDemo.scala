fe91a0a01ca4e96a31d384f2d3329cb7f9ad5863
caiyida <caiyida@hikvision.com>
2018-12-11 10:42:47 +0800

    [MOD]代码格式化

--- src/main/scala/cn/com/pcalpha/spark_demo/example/LogDemo.scala
+++ src/main/scala/cn/com/pcalpha/spark_demo/example/LogDemo.scala
@@ -6,27 +6,26 @@ import scala.util.matching.Regex
 
 object LogDemo {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("Word Count")setMaster("local");;
+    val conf = new SparkConf().setAppName("Word Count") setMaster ("local")
     val context = new SparkContext(conf);
-
     val input = context.textFile("testFile\\balarm.balarm-web.debug.log");
     //不同日志级别计数
-    input.filter(line=>isValidateLogLine(line))
-      .map(line=>line.split(" ")(1))
-      .map(word=>(word,1))
-      .reduceByKey((x,y)=>x+y)
-      .foreach(e=>println(e))
+    input.filter(line => isValidateLogLine(line))
+      .map(line => line.split(" ")(1))
+      .map(word => (word, 1))
+      .reduceByKey((x, y) => x + y)
+      .foreach(e => println(e))
   }
 
   //2018-11-17T09:44:27.273+08:00 DEBUG balarm-web [XNIO-2 task-175] [com.hikvision.ga.common.boot.cas.config.HikCasAuthenticationFilter] - serviceTicketRequest = false
   val PARTTERN: Regex ="""^(\S+) (\S+) (\S+) \[.+\] \[.+\] - (\S+) = (\S+)""".r;
   def isValidateLogLine(line: String): Boolean = {
-        val options = PARTTERN.findFirstMatchIn(line)
+    val options = PARTTERN.findFirstMatchIn(line)
 
-         if (options.isEmpty) {
-             false
-          } else {
-             true
+    if (options.isEmpty) {
+      false
+    } else {
+      true
     }
   }
 }
